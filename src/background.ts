ed800a293416e2b7983916d7efa09ed3c25f9ee0
John Jablonski <john.jablonski@eventstag.com>
2016-11-14 10:53:56 +0000

    Render and persist state of recording

--- src/background.ts
+++ src/background.ts
@@ -20,59 +20,116 @@ let stream: MediaStream = null;
 let recorder = null;
 let chunks: Array<Blob> = null;
 
+const helper = {
+    state: function () {
+        if (recorder && recorder.state === "recording") {
+            return "recording";
+        } else {
+            return "init";
+        }
+    },
+    record: function (): boolean {
+        console.log("Starting stream");
+        chrome.tabCapture.capture(captureOptions, function (s: MediaStream) {
+            stream = s;
+            chunks = [];
+            recorder = new MediaRecorder(stream, recorderOptions);
+            recorder.ondataavailable = function (e) {
+                chunks.push(e.data);
+            };
+            recorder.start();
+            console.log("Stream started");
+        });
+
+        return true;
+    },
+    stop: function (): boolean {
+        if (!stream) {
+            console.log("Stream not started");
+            return false;
+        }
+
+        console.log("Stopping stream");
+        stream.getVideoTracks()[0].stop();
+        recorder.stop();
+        console.log("Stream stopped");
+
+        return true;
+    },
+    save: function (): boolean {
+        if (chunks.length === 0) {
+            console.log("No data saved yet");
+            return false;
+        }
+
+        let blob = new Blob(chunks, {type: chunks[0].type});
+
+        chrome.downloads.download({
+                url: URL.createObjectURL(blob),
+                filename: "video.webm",
+                saveAs: true
+            },
+            function (downloadId) {
+                console.log(`Download with id = ${downloadId} started`);
+            }
+        );
+
+        return true;
+    }
+};
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    let response = {
+        state: "init"
+    };
+
     switch (message.action) {
         case "log":
             console.log.apply(console, message.args);
-
             break;
 
-        case "captureStart":
-            console.log("Starting stream");
-            chrome.tabCapture.capture(captureOptions, function (s: MediaStream) {
-                stream = s;
-                chunks = [];
-                recorder = new MediaRecorder(stream, recorderOptions);
-                recorder.ondataavailable = function (e) {
-                    chunks.push(e.data);
-                };
-                recorder.start();
-                console.log("Stream started");
-            });
+        case "state":
+            response.state = helper.state();
 
+            sendResponse(response);
             break;
 
-        case "captureStop":
-            if (!stream) {
-                console.log("Stream not started");
-                break;
+        case "record":
+            if (helper.record()) {
+                response.state = "recording";
             }
 
-            console.log("Stopping stream");
-            stream.getVideoTracks()[0].stop();
-            recorder.stop();
-            console.log("Stream stopped");
+            sendResponse(response);
+            break;
 
+        case "stop":
+            if (helper.stop()) {
+                response.state = "stopped";
+            }
+
+            sendResponse(response);
             break;
 
         case "save":
-            if (chunks.length === 0) {
-                console.log("No data saved yet");
-                break;
+            if (helper.stop()) {
+                response.state = "saved";
             }
 
-            let blob = new Blob(chunks, {type: chunks[0].type});
+            sendResponse(response);
+            break;
 
-            chrome.downloads.download({
-                    url: URL.createObjectURL(blob),
-                    filename: "video.webm",
-                    saveAs: true
-                },
-                function (downloadId) {
-                    console.log(`Download with id = ${downloadId} started`);
+        case "stopAndSave":
+            if (helper.stop()) {
+                response.state = "stopped";
+
+                if (helper.save()) {
+                    response.state = "init";
+
+                    sendResponse(response);
                 }
-            );
+            }
 
+            sendResponse(response);
             break;
 
         default:
--- src/popup.ts
+++ src/popup.ts
@@ -1,19 +1,34 @@
 const recordButton = document.getElementById("record");
 const stopButton = document.getElementById("stop");
-const saveButton = document.getElementById("save");
 
 const log = function (...args: any[]) {
-    chrome.runtime.sendMessage({ action: "log", args: args});
+    chrome.runtime.sendMessage({action: "log", args: args});
 };
 
+const renderState = function (state) {
+    if (state === "init") {
+        recordButton.classList.remove("hidden");
+        stopButton.classList.add("hidden");
+    } else if (state === "recording") {
+        recordButton.classList.add("hidden");
+        stopButton.classList.remove("hidden");
+    } else {
+        throw new Error(`Invalid state "${state}"`);
+    }
+};
+
+chrome.runtime.sendMessage({action: "state"}, function (response) {
+    renderState(response.state);
+});
+
 recordButton.addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "captureStart" });
+    chrome.runtime.sendMessage({action: "record"}, function (response) {
+        renderState(response.state);
+    });
 });
 
 stopButton.addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "captureStop" });
-});
-
-saveButton.addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "save" });
+    chrome.runtime.sendMessage({action: "stopAndSave"}, function (response) {
+        renderState(response.state);
+    });
 });
