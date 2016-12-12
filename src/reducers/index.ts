d2fc25adbf92c67a9ab65f7bb254e2c1c09a5984
John Jablonski <john.jablonski@eventstag.com>
2016-12-12 22:08:54 +0000

    React-Redux implementation WIP

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 
 const popupApp = combineReducers({
+    action: (state, action) => {
+        console.log(state, action);
+        return {};
+    }
 });
 
 export default popupApp;
